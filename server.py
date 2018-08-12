7c4ae272fad6f663da85298f20bdb0b94a5db63a
Kushagra Srivastava <kushagra1403@gmail.com>
2018-08-12 19:46:00 +0530

    some changes

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
